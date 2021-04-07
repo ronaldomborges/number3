144ec62ce79920745eccadd3de57ea40fd03b893
ronaldomborges <65875791+ronaldomborges@users.noreply.github.com>
2021-04-07 20:06:05 -0300

    fim

--- sript.js
+++ sript.js
@@ -45,7 +45,7 @@ function verificar() {
           }
   }
   res.style.textAlign = 'center'
-  res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+  res.innerHTML = ` ${gênero} com ${idade} anos`
   res.appendChild(img)
 }
 }
